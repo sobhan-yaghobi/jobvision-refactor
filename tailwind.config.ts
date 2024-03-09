91c4e8994b67a4b55e57b1070062a470271c95da
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-09 22:55:26 +0330

    build : set the connfig file and edit color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -8,13 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsla(var(--light-primary))",
+        },
+        white: "hsl(var(--white))",
+        black: "hsl(var(--black))",
+        gray: {
+          DEFAULT: "hsl(var(--gray))",
+          foreground: "hsl(var(--light-gray))",
+          "light-x2": "hsl(var(--light-gray-2x))",
+          "light-3x": "hsl(var(--light-gray-3x))",
+        },
+        danger: {
+          DEFAULT: "hsl(var(--danger))",
+          foreground: "hsl(var(--light-danger))",
+        },
+        success: {
+          DEFUALT: "hsl(var(--success))",
+          foreground: "hsl(var(--light-success))",
+        },
+        wraning: "hsl(var(--warning))",
+        purple: "hsl(var(--purple))",
+        blue: {
+          light: "hsl(var(--light-blue))",
+        },
+        bacground: "hsl(var(--background))",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+}
+export default config
