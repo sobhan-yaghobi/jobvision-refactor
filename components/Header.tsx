ca0faec3d9e4f9da4586d4a8035026f9701e10d0
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-11 15:05:48 +0330

    edit : set dir rtl to the project

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 const Header = () => {
   return (
     <header>
-      <div className="w-full h-20 bg-muted">
+      <div className="bg-muted w-full h-20 flex items-center justify-between">
         <Logo width={130} height={130} variants="PrimaryText" />
       </div>
     </header>
