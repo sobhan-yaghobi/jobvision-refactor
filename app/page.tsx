e03995522a17224d709967076b9f7c7e670d9295
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-11 12:53:39 +0330

    build : install prisma and set the configs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image"
 
-export default function Home() {
-  return <></>
+export default async function Home() {
+  return (
+    <>
+      <h1>hellow world</h1>
+    </>
+  )
 }
