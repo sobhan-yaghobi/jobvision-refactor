327f862331c3ec1857879c567e205f76990cc931
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-12 22:39:58 +0330

    add : build sideBar for header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,53 @@
-import { LogIn } from "lucide-react"
+import { Menu, User } from "lucide-react"
 
 import NavigationMenu from "./Navbar"
 import { Button } from "./ui/button"
 import Logo from "./Logo"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 
 const Header = async () => {
   return (
     <header className="border-b border-solid border-2 border-muted w-full mb-4 z-50">
       <div className="h-20 flex items-center justify-between container">
         <div className="h-full flex">
-          <Logo width={130} height={130} variants="PrimaryText" />
-          <NavigationMenu className="mr-6" />
+          <Drawer>
+            <DrawerTrigger>
+              <Button variant={"fill"} className="ml-3 xl:hidden">
+                <Menu className="icon" />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+                <DrawerDescription>This action cannot be undone.</DrawerDescription>
+              </DrawerHeader>
+              <DrawerFooter>
+                <Button>Submit</Button>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+
+          <Logo width={130} height={130} variants="PrimaryText" className="w-24" />
+          <NavigationMenu className="hidden mr-6 xl:flex" />
         </div>
         <div className="h-full flex items-center">
           <Button className="bg-primary">
-            <LogIn className="btn-icon-l icon" />
-            ورود | ثبت نام
+            <User className="md:btn-icon-l icon" />
+            <span className="hidden md:block">ورود | ثبت نام</span>
           </Button>
-          <Button variant={"ghost"} className="relative mr-6">
+          <Button variant={"ghost"} className="hidden relative mr-6 xl:block">
             بخش کارفرمایان
             <span className="bg-secondary-foreground w-[.5px] h-full -right-3 top-0 absolute"></span>
           </Button>
