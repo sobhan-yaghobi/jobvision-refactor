4488f654b938aaf5a543f8e2beb3a2a9a3aa3d58
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-12 12:06:10 +0330

    edit :  complete the NavigationMenu component

--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { categoryWithCollection, provinceWithCity } from "@/types/utils.type"
 
-import * as React from "react"
+import React from "react"
 import Link from "next/link"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs"
 import { Button } from "./ui/button"
@@ -12,7 +12,6 @@ type NavigationMenuProps = {
   province: provinceWithCity[]
   category: categoryWithCollection[]
 }
-
 const NavigationMenu: React.FC<NavigationMenuProps> = ({ className, province, category }) => (
   <>
     <ul className={cn("flex", className)}>
@@ -45,10 +44,10 @@ const NavigationMenu: React.FC<NavigationMenuProps> = ({ className, province, ca
             </TabsTrigger>
           </TabsList>
           <TabsContent value="mostVisitedJobs">
-            <ListItem mode="visitedJobs" array={category} />
+            <List mode="visitedJobs" array={category} />
           </TabsContent>
           <TabsContent value="cities">
-            <ListItem mode="cities" array={province} />
+            <List mode="cities" array={province} />
           </TabsContent>
         </Tabs>
       </div>
@@ -56,66 +55,60 @@ const NavigationMenu: React.FC<NavigationMenuProps> = ({ className, province, ca
   </>
 )
 
-type ListItemProps =
+type ListProps =
   | { mode: "cities"; array: provinceWithCity[] }
   | { mode: "visitedJobs"; array: categoryWithCollection[] }
-
-const ListItem: React.FC<ListItemProps> = ({ mode, array }) => {
+const List: React.FC<ListProps> = ({ mode, array }) => {
   return array.length ? (
     <ul className="text-sm bg-white w-full columns-4 absolute top-12 bottom-0 left-0 right-0 cursor-default rounded-b-xl overflow-y-auto">
       {mode === "cities"
         ? array.map((province) => (
-            <li key={`list-item-${province.id}`} className="overflow-hidden max-h-fit p-2">
-              <div className="text-jv-lightGray dana-bold w-full h-full flex flex-col px-3 py-1 hover:text-primary ">
-                <Link
-                  href={`jobs/province-${province.id}`}
-                  className="relative mb-1 after:content-[''] after:absolute after:top-0 after:-right-2 after:w-1 after:h-full after:bg-primary after:rounded-sm"
-                >
-                  {province.name}
-                </Link>
-                {province.city ? (
-                  <ul className="w-full px-3 cursor-default dana">
-                    {province.city.map((cit) => (
-                      <li className="w-full flex items-center mt-2 first:mt-0">
-                        <Link
-                          href={`jobs/city-${cit.id}`}
-                          className="text-secondary w-full inline-block hover:text-primary"
-                        >
-                          {cit.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                ) : null}
-              </div>
-            </li>
+            <Item
+              key={`list-item-${province.id}`}
+              itemHref={`jobs/province-${province.id}`}
+              itemName={province.name}
+            >
+              {province.city ? (
+                <ul className="w-full px-3 cursor-default dana">
+                  {province.city.map((cit) => (
+                    <li key={cit.id} className="w-full flex items-center mt-2 first:mt-0">
+                      <Link
+                        href={`jobs/city-${cit.id}`}
+                        className="text-secondary w-full inline-block hover:text-primary"
+                      >
+                        {cit.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : null}
+            </Item>
           ))
         : mode === "visitedJobs"
         ? array.map((category) => (
-            <li key={`list-item-${category.id}`} className="overflow-hidden max-h-fit p-2">
-              <div className="text-jv-lightGray dana-bold w-full h-full flex flex-col px-3 py-1 hover:text-primary ">
-                <Link
-                  href={`jobs/category-${category.id}`}
-                  className="relative mb-1 after:content-[''] after:absolute after:top-0 after:-right-2 after:w-1 after:h-full after:bg-primary after:rounded-sm"
-                >
-                  {category.name}
-                </Link>
-                {category.category_collection ? (
-                  <ul className="w-full px-3 cursor-default dana">
-                    {category.category_collection.map((collection) => (
-                      <li className="w-full flex items-center mt-2 first:mt-0">
-                        <Link
-                          href={`jobs/category_collection-${collection.link}`}
-                          className="text-secondary w-full inline-block hover:text-primary"
-                        >
-                          {collection.name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                ) : null}
-              </div>
-            </li>
+            <Item
+              key={`list-item-${category.id}`}
+              itemHref={`jobs/category-${category.id}`}
+              itemName={category.name}
+            >
+              {category.category_collection ? (
+                <ul className="w-full px-3 cursor-default dana">
+                  {category.category_collection.map((collection) => (
+                    <li
+                      key={`collection-item-${collection.id}`}
+                      className="w-full flex items-center mt-2 first:mt-0"
+                    >
+                      <Link
+                        href={`jobs/category_collection-${collection.link}`}
+                        className="text-secondary w-full inline-block hover:text-primary"
+                      >
+                        {collection.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              ) : null}
+            </Item>
           ))
         : null}
     </ul>
@@ -123,5 +116,23 @@ const ListItem: React.FC<ListItemProps> = ({ mode, array }) => {
     <h3 className="text-destructive">آیتمی وجود ندازد</h3>
   )
 }
+
+type ItemProps = {
+  itemHref: string
+  itemName: string
+}
+const Item: React.FC<React.PropsWithChildren<ItemProps>> = ({ itemHref, itemName, children }) => (
+  <li className="overflow-hidden max-h-fit p-2">
+    <div className="text-jv-lightGray dana-bold w-full h-full flex flex-col px-3 py-1 hover:text-primary ">
+      <Link
+        href={itemHref}
+        className="relative mb-1 after:content-[''] after:absolute after:top-0 after:-right-2 after:w-1 after:h-full after:bg-primary after:rounded-sm"
+      >
+        {itemName}
+      </Link>
+      {children}
+    </div>
+  </li>
+)
 
 export default NavigationMenu
