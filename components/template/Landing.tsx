35fdb1d8e3f12b896366aeab7eb4cc12ec7a6b22
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-13 19:20:53 +0330

    edit : change some style of landing

--- components/template/Landing.tsx
+++ components/template/Landing.tsx
@@ -3,7 +3,7 @@ import Map from "./Map"
 
 const Landing = () => {
   return (
-    <div className="h-auto grid grid-cols-1 grid-rows-2 justify-between relative pt-5 z-10 md:h-current lg:grid-cols-2">
+    <div className="h-auto grid grid-cols-1 grid-rows-3 justify-between relative z-10 md:h-current lg:grid-cols-2">
       <div className="right-landing flex flex-col justify-center col-span-1 row-span-1 text-center lg:col-span-1 lg:row-span-2 lg:text-right">
         <h1 className="text-2xl lg:text-3xl dana-bold">
           <span className="text-primary lg:ml-2">38,346</span> آگهی شغلی در
@@ -21,7 +21,7 @@ const Landing = () => {
         {/* {WindowsSize.innerWidth <= 768 ? null : <MapCircle />} */}
       </div>
       <div className="col-span-2 row-span-1 h-full">
-        <div className="w-full h-full flex flex-col items-start justify-center">
+        <div className="w-full h-full flex flex-col items-start">
           <h2 className="mb-5 pr-2 text-xl">دنبال چه شغلی می‌گردید؟</h2>
           {/* <SearchFrom /> */}
         </div>
