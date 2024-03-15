3750c2db1dec71e1978238cdf530979552f9adf6
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-15 10:44:09 +0330

    add : build company box and create a slider of it in home page

---
+++ components/modules/CompanyBox.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import Image from "next/image"
+import Link from "next/link"
+import { MoveLeft, Star } from "lucide-react"
+import { Button } from "./ui/button"
+import { cn } from "@/lib/utils"
+
+type CompanyBoxType = {
+  className?: string
+}
+
+const CompanyBox: React.FC<CompanyBoxType> = ({ className }) => {
+  return (
+    <Card
+      className={cn("bg-muted w-[300px] h-[280px] flex flex-col justify-between p-3", className)}
+    >
+      <CardHeader className="h-16 center items-start p-0">
+        <Image
+          width={190}
+          height={100}
+          className="w-auto h-full"
+          src="/images/Irancell_logo.webp"
+          alt="company-image"
+        />
+      </CardHeader>
+      <CardTitle className="my-3">MTN Irancell</CardTitle>
+      <CardContent className="p-0">
+        <div className="flex items-center gap-3">
+          <Star className="icon-lg fill-yellow-500 stroke-black stroke-1" stroke="1px" />
+          <span className="text-lg">3.5</span>
+        </div>
+        <Link href={"/jobs"} className="w-full flex items-center gap-3 mt-3">
+          23 آگهی شغل فعال
+          <MoveLeft />
+        </Link>
+      </CardContent>
+      <CardFooter className="mt-3 p-0">
+        <Button className="w-full">دنبال کنید</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default CompanyBox
--- components/template/CompnaySlider.tsx
+++ components/template/CompnaySlider.tsx
@@ -1,14 +1,10 @@
 "use client"
 
 import React from "react"
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
 
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "../modules/ui/card"
-import { Button } from "../modules/ui/button"
-import { MoveLeft, Star } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
+import { useKeenSlider } from "keen-slider/react"
+import CompanyBox from "../modules/CompanyBox"
+import "keen-slider/keen-slider.min.css"
 
 const CompnaySlider: React.FC = () => {
   const [sliderRef] = useKeenSlider<HTMLDivElement>(
@@ -55,31 +51,7 @@ const CompnaySlider: React.FC = () => {
       {Array(10)
         .fill("")
         .map((_, index) => (
-          <Card className="keen-slider__slide  bg-muted w-[300px] h-[280px] flex flex-col justify-between p-3">
-            <CardHeader className="h-16 center items-start p-0">
-              <Image
-                width={190}
-                height={100}
-                className="w-auto h-full"
-                src="/images/Irancell_logo.webp"
-                alt="company-image"
-              />
-            </CardHeader>
-            <CardTitle className="my-3">MTN Irancell</CardTitle>
-            <CardContent className="p-0">
-              <div className="flex items-center gap-3">
-                <Star className="icon-lg fill-yellow-500 stroke-black stroke-1" stroke="1px" />
-                <span className="text-lg">3.5</span>
-              </div>
-              <Link href={"/jobs"} className="w-full flex items-center gap-3 mt-3">
-                23 آگهی شغل فعال
-                <MoveLeft />
-              </Link>
-            </CardContent>
-            <CardFooter className="mt-3 p-0">
-              <Button className="w-full">دنبال کنید</Button>
-            </CardFooter>
-          </Card>
+          <CompanyBox key={`slider-company-box-${index + 1}`} className="keen-slider__slide" />
         ))}
     </div>
   )
