8d50d53e35b7f33182bab5ceb5049bc7511e09a0
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-17 10:53:03 +0330

    add & edit : add button for slider and responsive it

--- components/template/CompnaySlider.tsx
+++ components/template/CompnaySlider.tsx
@@ -1,18 +1,35 @@
 "use client"
 
-import React from "react"
+import React, { useState } from "react"
+
+import { ChevronLeft, ChevronRight } from "lucide-react"
 
 import { useKeenSlider } from "keen-slider/react"
+import { Button } from "../modules/ui/button"
 import CompanyBox from "../modules/CompanyBox"
+
 import "keen-slider/keen-slider.min.css"
 
 const CompnaySlider: React.FC = () => {
-  const [sliderRef] = useKeenSlider<HTMLDivElement>(
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>(
     {
-      loop: true,
-      slides: {
-        perView: 5,
-        spacing: 15,
+      slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel)
+      },
+      breakpoints: {
+        "(min-width: 300px)": {
+          slides: { perView: 1, spacing: 5 },
+        },
+        "(min-width: 768px)": {
+          slides: { perView: 2, spacing: 10 },
+        },
+        "(min-width: 1024px)": {
+          slides: { perView: 3, spacing: 10 },
+        },
+        "(min-width: 1280px)": {
+          slides: { perView: 5, spacing: 15 },
+        },
       },
     },
     [
@@ -26,7 +43,7 @@ const CompnaySlider: React.FC = () => {
           clearTimeout(timeout)
           if (mouseOver) return
           timeout = setTimeout(() => {
-            Object.is(null, slider) ? slider?.next() : null
+            slider.next()
           }, 2000)
         }
         slider.on("created", () => {
@@ -47,12 +64,32 @@ const CompnaySlider: React.FC = () => {
     ]
   )
   return (
-    <div ref={sliderRef} className="keen-slider py-6">
-      {Array(10)
-        .fill("")
-        .map((_, index) => (
-          <CompanyBox key={`slider-company-box-${index + 1}`} className="keen-slider__slide" />
-        ))}
+    <div className="navigation-wrapper">
+      <div ref={sliderRef} className="keen-slider py-6">
+        {Array(10)
+          .fill("")
+          .map((_, index) => (
+            <CompanyBox key={`slider-company-box-${index + 1}`} className="keen-slider__slide" />
+          ))}
+      </div>
+      {instanceRef.current && (
+        <>
+          <Button
+            className="ml-3"
+            onClick={(e: any) => e.stopPropagation() || instanceRef.current?.next()}
+            disabled={currentSlide === instanceRef.current.track.details.slides.length - 1}
+          >
+            <ChevronRight />
+          </Button>
+          <Button
+            className=""
+            onClick={(e: any) => e.stopPropagation() || instanceRef.current?.prev()}
+            disabled={currentSlide === 0}
+          >
+            <ChevronLeft />
+          </Button>
+        </>
+      )}
     </div>
   )
 }
