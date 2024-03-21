f81b843e71e20ba272428f0f2b0c3e7fd684df9e
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-21 18:45:49 +0330

    add : build the sidebar

--- components/template/dashboard/Sidebar.tsx
+++ components/template/dashboard/Sidebar.tsx
@@ -17,6 +17,10 @@ import {
   AccordionTrigger,
 } from "@/components/modules/ui/accordion"
 import Title from "@/components/modules/Title"
+import { cn } from "@/lib/utils"
+import { Card } from "@/components/modules/ui/card"
+import Image from "next/image"
+import { Button } from "@/components/modules/ui/button"
 
 type TypeSidebarItem = {
   label: string
@@ -37,25 +41,25 @@ const sidebarItems: TypeSidebarItem[] = [
     children: [
       {
         label: "قبول شده ها",
-        icon: <CheckCheck className="icon" />,
+        icon: <CheckCheck className="icon stroke-green-600/90" />,
         href: "request_accept",
         parent_href: "requests",
       },
       {
         label: "رد شده ها",
-        icon: <GitPullRequestClosed className="icon" />,
+        icon: <GitPullRequestClosed className="icon stroke-destructive" />,
         href: "request_reject",
         parent_href: "requests",
       },
       {
         label: "در حال انتظار",
-        icon: <AlarmClock className="icon" />,
+        icon: <AlarmClock className="icon stroke-yellow-400/90" />,
         href: "request_waiting",
         parent_href: "requests",
       },
       {
         label: "تمامی درخواست ها",
-        icon: <GitPullRequest className="icon" />,
+        icon: <GitPullRequest className="icon stroke-sec" />,
         href: "request_all",
         parent_href: "requests",
       },
@@ -63,29 +67,38 @@ const sidebarItems: TypeSidebarItem[] = [
   },
 ]
 
+const className = {
+  item: "flex w-full py-2 px-1 rounded-sm hover:bg-primary/20 cursor-pointer",
+  itemActive: "bg-primary/20",
+}
+
 const Sidebar = () => {
   return (
-    <div>
-      <Title size={"md"} className="text-primary mb-6">
-        <h2>جاب ویژن</h2>
-      </Title>
+    <div className="h-full flex flex-col justify-between">
       <Accordion type="single" collapsible>
+        <Title size={"md"} className="text-primary mb-6">
+          <h2>جاب ویژن</h2>
+        </Title>
         <ul className="flex flex-col gap-3">
           {sidebarItems.map((item) => (
             <Fragment key={item.href}>
               {item.children ? (
                 <AccordionItem className="border-none" value={item.href}>
-                  <AccordionTrigger className="!no-underline py-0 w-full">
-                    <SidebarTitle icon={item.icon} label={item.label} />
+                  <AccordionTrigger className={cn("!no-underline", className.item)}>
+                    <div className="flex">
+                      <SidebarTitle icon={item.icon} label={item.label} />
+                    </div>
                   </AccordionTrigger>
                   <AccordionContent className="flex flex-col gap-3 mt-3 mr-6">
                     {item.children.map((subItem) => (
-                      <SidebarTitle icon={subItem.icon} label={subItem.label} />
+                      <div className={className.item}>
+                        <SidebarTitle icon={subItem.icon} label={subItem.label} />
+                      </div>
                     ))}
                   </AccordionContent>
                 </AccordionItem>
               ) : (
-                <div className="flex">
+                <div className={className.item}>
                   <SidebarTitle icon={item.icon} label={item.label} />
                 </div>
               )}
@@ -93,6 +106,14 @@ const Sidebar = () => {
           ))}
         </ul>
       </Accordion>
+      <Card className="center flex-col py-2 px-3">
+        <Image width={150} height={150} src={"/images/report.webp"} alt="report-image" />
+        <Title className="w-auto">
+          <h4>گزارش سالانه</h4>
+        </Title>
+        <p className="text-xs my-2">همین الان از گزارش سالیانه مطلع شوید</p>
+        <Button size={"sm"}>دانلود</Button>
+      </Card>
     </div>
   )
 }
@@ -103,10 +124,10 @@ type SidebarTitleProps = {
 }
 const SidebarTitle: React.FC<SidebarTitleProps> = ({ icon, label }) => {
   return (
-    <div className="flex items-center">
+    <>
       {icon}
       <p className="mr-3">{label}</p>
-    </div>
+    </>
   )
 }
 
