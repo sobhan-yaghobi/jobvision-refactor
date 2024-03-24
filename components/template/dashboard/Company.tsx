38828d535e641bfee90592f2429266b71a66b199
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-03-24 17:11:53 +0330

    add : build the company schema and connect it to company form

--- components/template/dashboard/Company.tsx
+++ components/template/dashboard/Company.tsx
@@ -1,8 +1,12 @@
 "use client"
 
-import React from "react"
-import Persian_cl from "react-date-object/calendars/persian"
+import React, { useEffect, useRef, useState } from "react"
+
 import { DateObject } from "react-multi-date-picker"
+import { TypeCompany, companySchema } from "@/validation/zod.validations"
+import persian from "react-date-object/calendars/persian"
+import persian_fa from "react-date-object/locales/persian_fa"
+import gregorian_en from "react-date-object/locales/gregorian_en"
 
 import {
   Building2,
@@ -21,15 +25,58 @@ import { Textarea } from "@/components/modules/ui/textarea"
 import { Button } from "@/components/modules/ui/button"
 import Title from "@/components/modules/Title"
 import Calender from "@/components/modules/Calender"
+import { getLastMessage } from "@/lib/utils"
 
 const Company = () => {
-  const [date, setDate] = React.useState<Date>()
+  const formRef = useRef<HTMLFormElement>(null)
+  const [errs, setErrs] = useState<{ path: string; message: string }[]>()
+
+  const clientAction = (formData: FormData) => {
+    const companyObject: TypeCompany = {
+      name: formData.get("name") as string,
+      location: formData.get("location") as string,
+      logo: formData.get("logo") as string,
+      score_company: 4.3,
+      score_popularity: 4.6,
+      score_experience_of_job_seekers: 5,
+      score_responsiveness: 2,
+      website: formData.get("website") as string,
+      description: formData.get("description") as string,
+      slogan: formData.get("slogan") as string,
+      organization_employ: Number(formData.get("organization_employ")),
+      industry: formData.get("industry") as string,
+      established_year: new Date(
+        new DateObject({
+          date: formData.get("established_year") as string,
+          calendar: persian,
+          locale: persian_fa,
+        })
+          .convert(undefined, gregorian_en)
+          .format()
+      ),
+      type_of_activity: formData.get("type_of_activity") as string,
+    }
+
+    const resualt = companySchema.safeParse(companyObject)
+    if (!resualt.success) {
+      setErrs(
+        resualt.error.issues.map((err) => ({
+          message: err.message as string,
+          path: err.path.at(0) as string,
+        }))
+      )
+    }
+  }
+
+  useEffect(() => {
+    console.log("err", errs)
+  }, [errs])
   return (
     <>
       <Title>
         <h3>ویرایش اطلاعات شرکت</h3>
       </Title>
-      <form>
+      <form ref={formRef} action={clientAction}>
         <div className="flex mt-6">
           <div className="bg-muted w-24 h-24 center ml-3 rounded-sm shadow-lg">
             <span className="text-primary morabba">جاب ویژن</span>
@@ -38,8 +85,15 @@ const Company = () => {
             <InputMessage
               icon={<Image className="icon-stroke-light" />}
               wrapperClassName="w-full flex-row-reverse"
-              placeholder="لینک لگو شرکت.."
+              placeholder="لینک لوگو شرکت.."
               name="logo"
+              message={
+                getLastMessage({
+                  array: errs,
+                  key: "path",
+                  main_id: "logo",
+                })?.message
+              }
             />
             <p className="text-muted-foreground w-1/2 mt-3 text-xs">
               پیشنهاد میشود مقدار پیکسل لوگو شرکت 800 * 800 و فرمت عکس JPG یا PNG باشد و همچنین فرمت
@@ -54,7 +108,14 @@ const Company = () => {
             icon={<Building2 className="icon-stroke-light" />}
             wrapperClassName="w-full"
             placeholder="برای مثال جاب ویژن"
-            name="company_name"
+            name="name"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "name",
+              })?.message
+            }
           />
         </div>
 
@@ -65,6 +126,13 @@ const Company = () => {
             wrapperClassName="w-full"
             placeholder="برای مثال تهران ، بهارستان"
             name="location"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "location",
+              })?.message
+            }
           />
         </div>
 
@@ -75,6 +143,13 @@ const Company = () => {
             wrapperClassName="w-full"
             placeholder="برای مثال www.jobvision.com"
             name="website"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "website",
+              })?.message
+            }
           />
         </div>
 
@@ -83,8 +158,17 @@ const Company = () => {
           <Textarea
             className="max-h-32"
             placeholder="سخنی از سمت شرکت شما برای جویندگان شغل ..."
-            name="about_us"
+            name="description"
           />
+          <p className="mt-2 text-destructive text-xs">
+            {
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "description",
+              })?.message
+            }
+          </p>
         </div>
 
         <div className="mt-6">
@@ -94,6 +178,13 @@ const Company = () => {
             wrapperClassName="w-full"
             placeholder="برای مثال ، متفاوت باش"
             name="slogan"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "solgan",
+              })?.message
+            }
           />
         </div>
 
@@ -103,7 +194,14 @@ const Company = () => {
             icon={<MonitorDot className="icon-stroke-light" />}
             wrapperClassName="w-full"
             placeholder="برای مثال استخدام آنلاین"
-            name="activity_type"
+            name="type_of_activity"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "type_of_activity",
+              })?.message
+            }
           />
         </div>
 
@@ -114,6 +212,13 @@ const Company = () => {
             wrapperClassName="w-full"
             placeholder="برای مثال کاریابی آنلاین در ایران"
             name="industry"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "industry",
+              })?.message
+            }
           />
         </div>
 
@@ -125,7 +230,14 @@ const Company = () => {
             icon={<Users className="icon-stroke-light" />}
             wrapperClassName="w-full"
             placeholder="برای مثال کاریابی آنلاین در ایران"
-            name="industry"
+            name="organization_employ"
+            message={
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "organization_employ",
+              })?.message
+            }
           />
         </div>
 
@@ -134,9 +246,18 @@ const Company = () => {
           <Calender
             icon={<CalendarIcon className="icon-stroke-light" />}
             containerClassName="w-full *:h-10 *:cursor-pointer"
-            placeholder={`برای مثال ${new DateObject().convert(Persian_cl)}`}
-            name="establish"
+            placeholder={`برای مثال ${new DateObject().convert(persian)}`}
+            name="established_year"
           />
+          <p className="mt-2 text-destructive text-xs">
+            {
+              getLastMessage({
+                array: errs,
+                key: "path",
+                main_id: "established_year",
+              })?.message
+            }
+          </p>
         </div>
 
         <Button className="mt-6 w-full">ثبت شرکت</Button>
--- validation/zod.validations.ts
+++ validation/zod.validations.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-const signInSchema = z.object({
+export const signInSchema = z.object({
   email: z.string().trim().email("ایمیل معتبر نیست").min(1, "ایمیل اجباری میباشد"),
   password: z
     .string()
@@ -10,4 +10,20 @@ const signInSchema = z.object({
 })
 export type TypeSignIn = z.infer<typeof signInSchema>
 
-export { signInSchema, }
+export const companySchema = z.object({
+  name: z.string().trim().min(1, "نام اجباری میباشد"),
+  location: z.string().trim().min(1, "موقعیت اجباری میباشد"),
+  logo: z.string().url().trim().min(1, "لینک لوگو اجباری میباشد"),
+  score_company: z.number(),
+  score_popularity: z.number(),
+  score_experience_of_job_seekers: z.number(),
+  score_responsiveness: z.number(),
+  website: z.string().url().trim().min(1, "لینک وب سایت اجباری میباشد"),
+  description: z.string().trim().min(1, "درباره شرکت اجباری میباشد"),
+  slogan: z.string().trim().min(1, "شعار شرکت اجباری میباشد"),
+  organization_employ: z.number().min(1, "تعداد کارمندان شرکت اجباری میباشد"),
+  type_of_activity: z.string().trim().min(1, "نوع فعالیت اجباری میباشد"),
+  industry: z.string().trim().min(1, "نوع صنعت اجباری میباشد"),
+  established_year: z.date(),
+})
+export type TypeCompany = z.infer<typeof companySchema>
