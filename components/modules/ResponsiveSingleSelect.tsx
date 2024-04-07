131786a129ae04102e5314f5674e74c6ee76739f
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-04-07 21:43:58 +0330

    remove the useSize from responsiveSingleSelect

--- components/modules/ResponsiveSingleSelect.tsx
+++ components/modules/ResponsiveSingleSelect.tsx
@@ -2,21 +2,19 @@
 
 import SingleSelect, { SingleSelectProps } from "./SingleSelect"
 import { Drawer, DrawerContent, DrawerTrigger } from "./ui/drawer"
-import useSize from "@/hook/useSize"
 
 const ResponsiveSingleSelect: React.FC<React.PropsWithChildren<SingleSelectProps>> = (props) => {
-  const { width } = useSize()
-
   return (
     <>
-      {width >= 1024 ? (
-        <SingleSelect {...props} />
-      ) : (
+      <div className="lg:hidden">
         <Drawer>
           <DrawerTrigger asChild>{props.trigger}</DrawerTrigger>
           <DrawerContent className="px-3 py-2">{props.children}</DrawerContent>
         </Drawer>
-      )}
+      </div>
+      <div className="hidden lg:block">
+        <SingleSelect {...props} />
+      </div>
     </>
   )
 }
