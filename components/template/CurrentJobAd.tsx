ff0862bd3ddb4e7593b64edd346e6d4a4bd9dafb
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-04-11 09:01:42 +0330

    edit & fix : complete the currentJobAd and fix some styles

--- components/template/CurrentJobAd.tsx
+++ components/template/CurrentJobAd.tsx
@@ -7,10 +7,13 @@ import useSize from "@/hook/useSize"
 import TimeGenerator from "../modules/TimeGenerator"
 import { v4 as uuid } from "uuid"
 import { getTime } from "@/utils/utils.function"
+import { toast } from "../modules/ui/use-toast"
+
+import sendCv from "@/app/action/cv/sendCv"
 
 import { ad } from "@/types/utils.type"
 
-import { ExternalLink, Heart, Inbox, Speech, Users, X } from "lucide-react"
+import { ExternalLink, Heart, Inbox, Speech, Users } from "lucide-react"
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../modules/ui/tabs"
 import { Drawer, DrawerContent } from "../modules/ui/drawer"
@@ -79,101 +82,119 @@ const CurrentJobAd: React.FC = () => {
     setCurrent(undefined)
     setIsShow(false)
   }
+  const clientAction = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (current) {
+      event.stopPropagation()
+      const cvResault = await sendCv(current)
+      if (cvResault.status) {
+        return toast({ title: cvResault.message, variant: "default" })
+      }
+      return toast({ title: cvResault.message, variant: "destructive" })
+    }
+  }
 
-  const Wrapper = width >= 1024 ? Fragment : DrawerContent
+  const Wrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
+    return width >= 1024 ? (
+      <>{children}</>
+    ) : (
+      <Drawer open={isShow} onClose={closeAction}>
+        <DrawerContent className="h-current" touchClassName="bg-muted-foreground mb-3">
+          {children}
+        </DrawerContent>
+      </Drawer>
+    )
+  }
 
   return (
     <>
       {current ? (
-        <Drawer open={isShow} onClose={closeAction}>
-          <Wrapper className="h-current" touchClassName="bg-muted-foreground mb-3">
-            <div className="w-full h-full overflow-y-auto bg-muted px-3 rounded-sm">
-              <div>
-                <div
-                  style={{
-                    backgroundImage: "url('/images/company-image.webp')",
-                  }}
-                  className="bg-[#11111165] bg-blend-multiply w-full h-36 rounded-sm bg-fixed bg-center bg-no-repeat bg-cover"
-                ></div>
-              </div>
-              <div className="bg-muted w-full h-28 flex justify-between sticky top-0 py-3 z-20">
-                <div className="w-9/12">
-                  <Title>
-                    <h3 className="mb-3">{current.name}</h3>
-                  </Title>
-                </div>
-                <div className="w-2/12 flex items-start justify-end">
-                  <Button>ارسال رزومه</Button>
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="w-full flex flex-col text-sm">
-                  <p>{`${current.company.location.city.name} , ${current.company.location.address}`}</p>
-                  <div className="mb-2 flex flex-wrap *:text-sm">
-                    {current.itern ? (
-                      <div className="box-info-type !mr-1 !p-0">امکان جذب کارآموز</div>
-                    ) : null}
-                    {current.telecommuting ? (
-                      <div className="box-info-type !mr-1 !p-0">امکان دورکاری</div>
-                    ) : null}
-                  </div>
-                  <div className="my-2 flex items-start justify-between">
-                    <div className="flex flex-col gap-3">
-                      <p>
-                        {`${current.price.min.toLocaleString("fa-ir")}${
-                          current.price.max ? ` - ${current.price.max.toLocaleString("fa-ir")}` : ""
-                        } `}
-                        تومان
-                      </p>
-                      <TimeGenerator dateInfo={getTime(current.created_at)} />
-                      <Button variant={"outline"}>مشاهده حقوق دریافتی افراد در مشاغل مشابه</Button>
-                    </div>
-                    <div className="flex text-2xl text-jv-primary">
-                      <ExternalLink className="icon-stroke-light btn-icon-l" />
-                      <Heart className="icon-stroke-light" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <Separator className="my-3" />
-              <div className="w-full flex items-center justify-start text-sm">
-                <div className="w-4/12 flex items-center fill-jv-lightGray2x">
-                  <Users className="icon-stroke-light" />
-                  <p className="mr-3 truncate">{current.company.organization_employ} نفر</p>
-                </div>
-                <div className="w-8/12 flex items-center">
-                  <Speech className="icon-stroke-light" />
-                  <p className="w-10/12 mr-3 truncate">{current.company.slogan}</p>
-                </div>
-              </div>
-              <Tabs
-                dir="rtl"
-                defaultValue={mainItemsBoxInfos.at(0)?.type}
-                className="h-full my-3 p-0 flex flex-col"
-              >
-                <TabsList variant={"secondary"} className="w-full sticky top-28 z-20">
-                  {mainItemsBoxInfos.map((item) => (
-                    <TabsTrigger
-                      className="p-5"
-                      key={`trigger-${item.type}`}
-                      variant={"secondary"}
-                      value={item.type}
-                    >
-                      {item.title}
-                    </TabsTrigger>
-                  ))}
-                </TabsList>
-                <div className="bg-background p-3">
-                  {mainItemsBoxInfos.map((item) => (
-                    <TabsContent className="h-full" key={`content-${item.type}`} value={item.type}>
-                      {item.component}
-                    </TabsContent>
-                  ))}
-                </div>
-              </Tabs>
+        <Wrapper>
+          <div className="w-full h-full overflow-y-auto bg-muted px-3 rounded-sm">
+            <div>
+              <div
+                style={{
+                  backgroundImage: "url('/images/company-image.webp')",
+                }}
+                className="bg-[#11111165] bg-blend-multiply w-full h-36 rounded-sm bg-fixed bg-center bg-no-repeat bg-cover"
+              ></div>
             </div>
-          </Wrapper>
-        </Drawer>
+            <div className="bg-muted w-full h-28 flex justify-between sticky top-0 py-3 z-20">
+              <div className="w-9/12">
+                <Title>
+                  <h3 className="mb-3">{current.name}</h3>
+                </Title>
+              </div>
+              <div className="w-2/12 flex items-start justify-end">
+                <Button onClick={clientAction}>ارسال رزومه</Button>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="w-full flex flex-col text-sm">
+                <p>{`${current.company.location.city.name} , ${current.company.location.address}`}</p>
+                <div className="mb-2 flex flex-wrap *:text-sm">
+                  {current.itern ? (
+                    <div className="box-info-type !mr-1 !p-0">امکان جذب کارآموز</div>
+                  ) : null}
+                  {current.telecommuting ? (
+                    <div className="box-info-type !mr-1 !p-0">امکان دورکاری</div>
+                  ) : null}
+                </div>
+                <div className="my-2 flex items-start justify-between">
+                  <div className="flex flex-col gap-3">
+                    <p>
+                      {`${current.price.min.toLocaleString("fa-ir")}${
+                        current.price.max ? ` - ${current.price.max.toLocaleString("fa-ir")}` : ""
+                      } `}
+                      تومان
+                    </p>
+                    <TimeGenerator dateInfo={getTime(current.created_at)} />
+                    <Button variant={"outline"}>مشاهده حقوق دریافتی افراد در مشاغل مشابه</Button>
+                  </div>
+                  <div className="flex text-2xl text-jv-primary">
+                    <ExternalLink className="icon-stroke-light btn-icon-l" />
+                    <Heart className="icon-stroke-light" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Separator className="my-3" />
+            <div className="w-full flex items-center justify-start text-sm">
+              <div className="w-4/12 flex items-center fill-jv-lightGray2x">
+                <Users className="icon-stroke-light" />
+                <p className="mr-3 truncate">{current.company.organization_employ} نفر</p>
+              </div>
+              <div className="w-8/12 flex items-center">
+                <Speech className="icon-stroke-light" />
+                <p className="w-10/12 mr-3 truncate">{current.company.slogan}</p>
+              </div>
+            </div>
+            <Tabs
+              dir="rtl"
+              defaultValue={mainItemsBoxInfos.at(0)?.type}
+              className="h-full my-3 p-0 flex flex-col"
+            >
+              <TabsList variant={"secondary"} className="w-full sticky top-28 z-20">
+                {mainItemsBoxInfos.map((item) => (
+                  <TabsTrigger
+                    className="p-5"
+                    key={`trigger-${item.type}`}
+                    variant={"secondary"}
+                    value={item.type}
+                  >
+                    {item.title}
+                  </TabsTrigger>
+                ))}
+              </TabsList>
+              <div className="bg-background p-3">
+                {mainItemsBoxInfos.map((item) => (
+                  <TabsContent className="h-full" key={`content-${item.type}`} value={item.type}>
+                    {item.component}
+                  </TabsContent>
+                ))}
+              </div>
+            </Tabs>
+          </div>
+        </Wrapper>
       ) : (
         <div className="bg-muted h-full w-full center flex-col p-3 rounded-sm">
           <Inbox className="w-52 h-52 stroke-muted-foreground stroke-1" />
