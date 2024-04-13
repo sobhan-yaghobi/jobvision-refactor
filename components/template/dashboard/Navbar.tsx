002b5d7eb1ed834c65ce99e9c70a8710b9d1bf12
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-04-13 15:18:38 +0330

    fix : the navbar bug when route change

--- components/template/dashboard/Navbar.tsx
+++ components/template/dashboard/Navbar.tsx
@@ -1,22 +1,43 @@
-import Title from "@/components/modules/Title"
-import { Button } from "@/components/modules/ui/button"
-import { Drawer, DrawerContent, DrawerTrigger } from "@/components/modules/ui/drawer"
-import { Menu, Rocket } from "lucide-react"
-import Link from "next/link"
-import React from "react"
-import QuickAccess from "./QuickAccess"
+"use client"
+
+import React, { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
+
 import { companyWithLocation } from "@/types/utils.type"
+
+import { Menu, Rocket } from "lucide-react"
+
 import { Sheet, SheetContent, SheetTrigger } from "@/components/modules/ui/sheet"
+import QuickAccess from "./QuickAccess"
 import Sidebar from "./Sidebar/Sidebar"
+import { Button } from "@/components/modules/ui/button"
+import Title from "@/components/modules/Title"
+import Link from "next/link"
 
 type NavbarProps = {
   company: companyWithLocation | null
 }
 
 const Navbar: React.FC<NavbarProps> = ({ company }) => {
+  const [isMenu, setIsMenu] = useState(false)
+  const [isQuick, setIsQuick] = useState(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    closeSheet()
+
+    return () => {
+      closeSheet()
+    }
+  }, [pathname])
+
+  const closeSheet = () => {
+    setIsMenu(false)
+    setIsQuick(false)
+  }
   return (
     <div className="flex items-center justify-between p-3 shadow-lg relative z-20">
-      <Sheet>
+      <Sheet open={isMenu} onOpenChange={setIsMenu}>
         <SheetTrigger asChild>
           <Button title="منو" size={"sm"} variant={"fill"}>
             <Menu className="icon" />
@@ -31,7 +52,7 @@ const Navbar: React.FC<NavbarProps> = ({ company }) => {
           <h2>جاب ویژن</h2>
         </Title>
       </Link>
-      <Sheet>
+      <Sheet open={isQuick} onOpenChange={setIsQuick}>
         <SheetTrigger asChild>
           <Button title="دسترسی سریع" size={"sm"} variant={"fill"}>
             <Rocket className="icon" />
