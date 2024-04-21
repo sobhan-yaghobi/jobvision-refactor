a764e46a4fcd60033f8829b46e9e6653c0206ce1
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-04-21 23:52:12 +0330

    fix : cache the ADs dashboard with swr package

--- components/template/dashboard/Ads.tsx
+++ components/template/dashboard/Ads.tsx
@@ -7,32 +7,25 @@ import { ad } from "@/types/utils.type"
 
 import AdsBox from "@/components/modules/dashboard/AdsBox"
 import Title from "@/components/modules/Title"
+import useSWR from "swr"
 
 const Ads: React.FC = () => {
-  const [adItems, setAdItems] = useState<ad[]>()
-  useEffect(() => {
-    const fetchAction = async () => {
-      const res = await fetch("/api/ad?query=me", {
-        method: "GET",
-      })
-
-      const data = await res.json()
-      setAdItems(data)
-    }
-
-    fetchAction()
-  }, [])
+  const {
+    data: adItems,
+    isLoading,
+    mutate,
+  } = useSWR("/api/ads/me", async () => {
+    const res = await fetch("/api/ad?query=me")
+    const adsData: ad[] = await res.json()
+    return adsData
+  })
 
   const removeAction = async (id: string) => {
     const res = await fetch("/api/ad", { method: "DELETE", body: JSON.stringify(id) })
     const data = await res.json()
     if (res.status === 201) {
       toast({ title: data.message, variant: "default" })
-      setAdItems((prev) =>
-        filter(prev, function (prevItem) {
-          return prevItem.id !== id
-        })
-      )
+      mutate()
       return
     }
     toast({ title: data.message, variant: "destructive" })
@@ -42,16 +35,12 @@ const Ads: React.FC = () => {
       <Title className="mb-6">
         <h3>آگهی ها</h3>
       </Title>
-      {adItems ? (
-        adItems.length ? (
-          adItems.map((item) => <AdsBox key={item.id} ad={item} remove={removeAction} />)
-        ) : (
-          <p className="bg-destructive-foreground text-destructive p-2 rounded-sm">
-            آگهی وجود ندارد
-          </p>
-        )
-      ) : (
+      {isLoading ? (
         <>در حال بارگذاری</>
+      ) : adItems?.length ? (
+        adItems.map((item) => <AdsBox key={item.id} ad={item} remove={removeAction} />)
+      ) : (
+        <p className="bg-destructive-foreground text-destructive p-2 rounded-sm">آگهی وجود ندارد</p>
       )}
     </div>
   )
