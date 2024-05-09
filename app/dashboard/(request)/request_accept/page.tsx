9d4e51016d17adc9f10470be6b2f024863c02388
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-05-10 00:49:09 +0330

    fix : rendering issue in request pages in dashboard

--- app/dashboard/(request)/request_accept/page.tsx
+++ app/dashboard/(request)/request_accept/page.tsx
@@ -1,44 +1,8 @@
-"use client"
-
 import React from "react"
-import useSWR from "swr"
+import AcceptRequests from "@/components/template/dashboard/Requests/AcceptRequests"
 
-import { CVWithAdWithUser } from "@/types/utils.type"
-
-import Title from "@/components/modules/Title"
-import CVRequest from "@/components/modules/dashboard/CVRequest"
-import CVRequestSkeleton from "@/components/modules/skeleton/CVRequest.skeleton"
-
-const page = () => {
-  const {
-    data: acceptCV,
-    mutate,
-    isLoading,
-  } = useSWR("api/request_accept", async () => {
-    const res = await fetch("/api/cv?status=accept")
-    const cvData: CVWithAdWithUser[] = await res.json()
-    return cvData
-  })
-  return (
-    <div>
-      <Title>
-        <h3>درخواست های قبول شده</h3>
-      </Title>
-      <div className="mt-3">
-        {isLoading ? (
-          Array(5)
-            .fill("")
-            .map((_, index) => <CVRequestSkeleton key={`last-cv-skeleton-${index}`} />)
-        ) : acceptCV?.length ? (
-          acceptCV.map((item) => (
-            <CVRequest mutate={mutate} cv={item} status={item.status} key={item.id} />
-          ))
-        ) : (
-          <>درخواستی وجود ندارد</>
-        )}
-      </div>
-    </div>
-  )
+const page: React.FC = () => {
+  return <AcceptRequests />
 }
 
 export default page
---
+++ components/template/dashboard/Requests/RejectReuqests.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import React from "react"
+import useSWR from "swr"
+
+import { CVWithAdWithUser } from "@/types/utils.type"
+
+import Title from "@/components/modules/Title"
+import CVRequest from "@/components/modules/dashboard/CVRequest"
+import CVRequestSkeleton from "@/components/modules/skeleton/CVRequest.skeleton"
+
+const RejectReuqests = () => {
+  const {
+    data: rejectCV,
+    mutate,
+    isLoading,
+  } = useSWR("api/request_all", async () => {
+    const res = await fetch("/api/cv?status=reject")
+    const cvData: CVWithAdWithUser[] = await res.json()
+    return cvData
+  })
+  return (
+    <div>
+      <Title>
+        <h3>درخواست های رد شده</h3>
+      </Title>
+      <div className="mt-3">
+        {isLoading ? (
+          Array(5)
+            .fill("")
+            .map((_, index) => <CVRequestSkeleton key={`last-cv-skeleton-${index}`} />)
+        ) : rejectCV?.length ? (
+          rejectCV.map((item) => (
+            <CVRequest mutate={mutate} cv={item} status={item.status} key={item.id} />
+          ))
+        ) : (
+          <>درخواستی وجود ندارد</>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default RejectReuqests
