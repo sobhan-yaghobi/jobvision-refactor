6c363e173dca33223501c28ed3520f2e396f9d58
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-05-10 00:49:09 +0330

    fix : rendering issue in request pages in dashboard

--- app/dashboard/(request)/request_waiting/page.tsx
+++ app/dashboard/(request)/request_waiting/page.tsx
@@ -1,44 +1,10 @@
 "use client"
 
 import React from "react"
-import useSWR from "swr"
-
-import { CVWithAdWithUser } from "@/types/utils.type"
-
-import Title from "@/components/modules/Title"
-import CVRequest from "@/components/modules/dashboard/CVRequest"
-import CVRequestSkeleton from "@/components/modules/skeleton/CVRequest.skeleton"
+import WaitingRequests from "@/components/template/dashboard/Requests/WaitingRequests"
 
 const page = () => {
-  const {
-    data: waitingCv,
-    mutate,
-    isLoading,
-  } = useSWR("api/request_all", async () => {
-    const res = await fetch("/api/cv?status=waiting")
-    const cvData: CVWithAdWithUser[] = await res.json()
-    return cvData
-  })
-  return (
-    <div>
-      <Title>
-        <h3>رزومه های در حال انتظار</h3>
-      </Title>
-      <div className="mt-3">
-        {isLoading ? (
-          Array(5)
-            .fill("")
-            .map((_, index) => <CVRequestSkeleton key={`last-cv-skeleton-${index}`} />)
-        ) : waitingCv?.length ? (
-          waitingCv.map((item) => (
-            <CVRequest mutate={mutate} cv={item} status={item.status} key={item.id} />
-          ))
-        ) : (
-          <>درخواستی وجود ندارد</>
-        )}
-      </div>
-    </div>
-  )
+  return <WaitingRequests />
 }
 
 export default page
