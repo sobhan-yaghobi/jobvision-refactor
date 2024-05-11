3f5e1814df674f9f80d8bc12f1bd50074f0551b2
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-05-11 17:28:04 +0330

    edit : remove test element in Compnay and change function condition in setImageCompany action

--- app/action/setImageCompany.ts
+++ app/action/setImageCompany.ts
@@ -11,17 +11,21 @@ const setImageCompany = async (logoFile: File, logoName?: string) => {
   if (logoFile && logoFile.size && user) {
     const buffer = Buffer.from(await logoFile.arrayBuffer())
     fileName = uuid() + "_" + logoFile.name
-    writeFile(path.join(process.cwd(), "public/uploads/" + fileName), buffer, (err) => {
-      if (!err) {
+    writeFile(path.join(process.cwd(), "public/uploads/" + fileName), buffer, async (err) => {
+      if (err) {
+        console.error("error on write file in [changeProfile.ts]", err)
+      } else {
         if (logoName) {
-          unlink(path.join(process.cwd(), "public/uploads", logoName), (err) => {
-            console.error("error on unlink a file in [changeProfile.ts]", err)
+          unlink(path.join(process.cwd(), "/public/uploads", logoName), (err) => {
+            if (err) {
+              console.error("error on unlink a file in [setImageCompany.ts]", err)
+            }
           })
         }
       }
     })
   }
-  return fileName.length ? fileName : undefined
+  return fileName
 }
 
 export default setImageCompany
