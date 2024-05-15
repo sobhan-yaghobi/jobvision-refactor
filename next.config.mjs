2229248fafd1f46245a3619703e615888dfcffd3
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-05-15 22:12:15 +0330

    change : the image config in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["xsuywgifuwiufdwmpzxr.supabase.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xsuywgifuwiufdwmpzxr.supabase.co",
+        pathname: "**",
+      },
+    ],
   },
   output: "standalone",
 }
