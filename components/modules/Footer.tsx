c66ccac0550ef3c89a0098663b3ba328edb38010
sobhan <sobhan.yaghobi.work@gmail.com>
2024-05-17 17:46:11 +0330

    change the footer and just remain the repo url

--- components/modules/Footer.tsx
+++ components/modules/Footer.tsx
@@ -14,18 +14,10 @@ import Title from "./Title"
 import Image from "next/image"
 import Link from "next/link"
 import { QuickAccessItemType } from "@/types/utils.type"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog"
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-secondary text-secondary-foreground overflow-hidden py-6">
+    <footer className="bg-secondary text-secondary-foreground overflow-hidden pt-6 pb-3">
       <div className="container">
         <section>
           <Title size={"md"}>
@@ -194,49 +186,29 @@ const Footer: React.FC = () => {
           </div>
         </section>
 
+        {/*//! ---------- Repo Url  */}
         <section className="text-center mt-3">
           <p>
             این وب‌سایت یک <span className="font-bold">کلون وب</span> از وب‌سایت رسمی است.
           </p>
           <div className="center mt-3">
-            <Dialog>
-              <DialogTrigger className="text-xs underline">
-                <Button variant={"ghost"}>اطلاعات بیشتر</Button>
-              </DialogTrigger>
-              <DialogContent className="rounded-sm">
-                <DialogHeader>
-                  <DialogTitle className="text-center">
-                    <Title size="lg" className="text-center">
-                      <h1>اطلاعیه کلون وب</h1>
-                    </Title>
-                  </DialogTitle>
-                </DialogHeader>
-                <DialogDescription className="text-center my-3">
-                  <p className="mb-3">
-                    مهم است توجه داشته باشید که این وب‌سایت هیچ ارتباطی با وب‌سایت رسمی ندارد. این
-                    فقط یک کلون توسط طرفداران از وب‌سایت رسمی است. هدف این وب‌سایت این است که به
-                    کاربران راهی برای دسترسی به اطلاعات و منابع موجود در وب‌سایت رسمی فراهم کند، اما
-                    با طراحی یا تجربه کاربری متفاوت. با این حال، لازم است توجه داشته باشید که این
-                    وب‌سایت منبع رسمی اطلاعات نیست. اگر نیاز به دسترسی به اطلاعات رسمی دارید، لطفاً
-                    وب‌سایت رسمی را مشاهده کنید
-                  </p>
-                  <Link
-                    className="text-primary underline"
-                    target="_blank"
-                    href={"https://jobvision.ir"}
-                  >
-                    وب سایت رسمی جاب ویژن
-                  </Link>
-                </DialogDescription>
-              </DialogContent>
-            </Dialog>
-            <span className="bg-muted w-0.5 h-10 mx-3"></span>
-            <Button variant={"link"}>
+            <Button variant={"outline"} className="text-black hover:shadow-xl">
               <Link
-                className="underline text-xs"
+                className="flex"
                 target="_blank"
                 href={"https://github.com/sobhan-yaghobi/jobvision-refactor"}
               >
+                <svg
+                  height="32"
+                  aria-hidden="true"
+                  viewBox="0 0 16 16"
+                  version="1.1"
+                  width="32"
+                  data-view-component="true"
+                  className="icon btn-icon-l"
+                >
+                  <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                </svg>
                 ریپو پروژه
               </Link>
             </Button>
